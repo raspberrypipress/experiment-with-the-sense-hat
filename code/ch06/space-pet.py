146de2e1d74a780b2d24fda37e6c2bfb25a34035
Brian Jepson <brian.jepson@raspberrypi.com>
2025-03-24 15:11:31 -0400

    reverted the pet1 definition

--- code/ch06/space-pet.py
+++ code/ch06/space-pet.py
@@ -13,9 +13,9 @@ pet1 = [
  e, e, e, e, e, e, e, e,
  p, e, e, e, e, e, e, e,
  e, p, e, e, p, e, p, e,
- e, p, g, g, p, y, y, e,
- e, g, g, g, y, w, y, g,
- e, g, g, g, g, y, y, e,
+ e, p, g, g, p, w, w, e,
+ e, g, g, g, w, y, w, y,
+ e, g, g, g, g, w, w, e,
  e, g, e, g, e, g, e, e,
  e, e, e, e, e, e, e, e
  ]
