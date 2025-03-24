ec703673fca302c5036a183dd3bc5571c0c1748f
Brian Jepson <brian.jepson@raspberrypi.com>
2025-03-24 15:10:20 -0400

    tweaks for consistency with text

--- code/ch06/space-pet.py
+++ code/ch06/space-pet.py
@@ -4,7 +4,7 @@ import time
 sense = SenseHat()
 
 p = (204, 0, 204) # Pink
-g = (0, 102, 102) # Gray
+g = (0, 102, 102) # Dark Green
 w = (200, 200, 200) # White
 y = (204, 204, 0) # Yellow
 e = (0, 0, 0) # Empty
@@ -13,9 +13,9 @@ pet1 = [
  e, e, e, e, e, e, e, e,
  p, e, e, e, e, e, e, e,
  e, p, e, e, p, e, p, e,
- e, p, g, g, p, w, w, e,
- e, g, g, g, w, y, w, y,
- e, g, g, g, g, w, w, e,
+ e, p, g, g, p, y, y, e,
+ e, g, g, g, y, w, y, g,
+ e, g, g, g, g, y, y, e,
  e, g, e, g, e, g, e, e,
  e, e, e, e, e, e, e, e
  ]
