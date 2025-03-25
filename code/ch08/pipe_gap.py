d13136d17aadc4a0a087a47a7dedcb6de17ae757
Brian Jepson <brian.jepson@raspberrypi.com>
2025-03-25 14:37:02 -0400

    another example

---
+++ code/ch08/pipe_gap.py
@@ -0,0 +1,22 @@
+from sense_hat import SenseHat
+from random import randint
+
+sense = SenseHat()
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+
+matrix = [[BLUE for column in range(8)] for row in range(8)]
+
+def flatten(matrix):
+    flattened = [pixel for row in matrix for pixel in row]
+    return flattened
+
+for row in matrix:
+    row[-1] = RED
+    gap = randint(1, 6)
+    matrix[gap][-1] = BLUE
+    matrix[gap + 1][-1] = BLUE
+    matrix[gap - 1][-1] = BLUE
+
+matrix = flatten(matrix)
+sense.set_pixels(matrix)
